9858a96691efd328125f044df1384fe9b7694d3d
Abhi Pillai <abhi.pillai1@gmail.com>
2017-05-13 10:26:53 -0400

    Add home page with blank html

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule Devmentor.PageController do
   use Devmentor.Web, :controller
 
+  def home(conn, _params) do
+    render conn, "home.html"
+  end
+
   def phoenix_home(conn, _params) do
     render conn, "phoenix_home.html"
   end
