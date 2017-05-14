9d272a8daa2cb14b5acee3417484be0a1252aa55
Abhi Pillai <abhi.pillai1@gmail.com>
2017-05-14 12:07:35 -0400

    Add new column for note_type and default to general

--- web/models/note.ex
+++ web/models/note.ex
@@ -5,6 +5,7 @@ defmodule Devmentor.Note do
 
   schema "notes" do
     field :body, :string
+    field :note_type, :string
 
     belongs_to :user, Devmentor.User
     belongs_to :mentorship, Devmentor.Mentorship
@@ -15,10 +16,20 @@ defmodule Devmentor.Note do
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
-    required_params = [:body, :mentorship_id, :user_id]
-    struct
-    |> cast(params, required_params)
+    required_params = [:body, :mentorship_id, :user_id, :note_type]
+    change =
+      struct
+      |> cast(params, required_params)
+
+    change =
+      case Map.has_key?(change, :note_type) do
+        false -> change |> put_change(:note_type, "general")
+        true -> change
+      end
+
+    change
     |> validate_required(required_params)
+    |> validate_inclusion(:note_type, ["general", "action_item"])
   end
 
   def by_mentorship(m_id) do
